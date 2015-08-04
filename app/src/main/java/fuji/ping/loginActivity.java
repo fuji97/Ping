bc8fc3def00da1becf386d44e54f8b4c316d1997
Federico Rapetti <fuji1097@gmail.com>
2015-08-04 02:38:09 +0200

    Action Bar removed

--- app/src/main/java/fuji/ping/loginActivity.java
+++ app/src/main/java/fuji/ping/loginActivity.java
@@ -1,11 +1,12 @@
 package fuji.ping;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class loginActivity extends AppCompatActivity {
+public class loginActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
